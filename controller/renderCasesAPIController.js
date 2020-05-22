004de3b330d5b5ee21d668fc50cc5d6aa7305d02
Naveed Sultan <32653946+naveed1224@users.noreply.github.com>
2020-05-21 22:38:31 -0400

    finished backend to get cases by day, week, month, total

--- controller/renderCasesAPIController.js
+++ controller/renderCasesAPIController.js
@@ -3,7 +3,7 @@ const CasesModel = require('../models/cases');
 exports.renderAllCases = (req, res, next) => {
     console.log(req.query.page)
     const currentPage = req.query.page || 1;
-    const perPage = 3;
+    const perPage = 10;
     let totalCases;
 
     CasesModel.find().countDocuments()
@@ -36,7 +36,7 @@ exports.renderSearchCases = (req, res, next) => {
     //     message: 'Successfully reached search engine API'
     // })
     const currentPage = req.query.page || 1;
-    const perPage = 3;
+    const perPage = 10;
     let totalCases;
     const queryTypeParser = {
         "4": "neighborhood",
@@ -86,3 +86,117 @@ exports.renderSearchCases = (req, res, next) => {
             next(err);
         })
 }
+
+
+exports.statsQueryController = (req, res, next) => {
+    console.log(req.query)
+    const statsQueryType = {
+
+    }
+    // documents today
+    if (req.query.statsType === "1") {
+        CasesModel.countDocuments({
+                "createdAt": {
+                    "$gte": new Date(new Date().setUTCHours(0, 0, 0, 0)).toISOString()
+                }
+            })
+            .then(result => {
+                res.status(200).json({
+                    message: "reached StatsQuery API",
+                    data: result
+                })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+    //cases this week
+    if (req.query.statsType === "2") {
+        let dateObj = {}
+        const today = new Date();
+        const startDay = 0;
+        const weekStart = new Date(today.getDate() - (7 + today.getDay() - startDay) % 7);
+        dateObj.weekStartDate = weekStart.toISOString()
+        CasesModel.countDocuments({
+                "createdAt": {
+                    "$gte": dateObj.weekStartDate
+                }
+            })
+            .then(result => {
+                res.status(200).json({
+                    message: "reached StatsQuery API",
+                    data: result
+                })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+    }
+    if (req.query.statsType === "3") {
+        let dateObj = {}
+        const today = new Date();
+        const monthStart = new Date(today.getFullYear(), today.getMonth(), 1);
+        dateObj.monthStartDate = monthStart.toISOString()
+        CasesModel.countDocuments({
+                "createdAt": {
+                    "$gte": dateObj.monthStartDate
+                }
+            })
+            .then(result => {
+                res.status(200).json({
+                    message: "reached StatsQuery API",
+                    data: result
+                })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+    if (req.query.statsType === "4") {
+        CasesModel.countDocuments({})
+            .then(result => {
+                res.status(200).json({
+                    message: "reached StatsQuery API",
+                    data: result
+                })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+}
+
+
+// CasesModel.aggregate([{
+//     "$match": {
+//         "createdAt": {
+//             // "$gte": new Date("2020-01-01"),
+//             // "$lt": new Date("2020-05-22")
+//             queryObj
+//         },
+//     }
+// },
+// {
+//     "$group": {
+//         "_id": {
+//             // "year": {
+//             //     "$year": "$createdAt"
+//             // },
+//             // "month": {
+//             //     "$month": "$createdAt"
+//             // },
+//             "week": {
+//                 "$week": "$createdAt"
+//             },
+//             "day": {
+//                 "$dayOfMonth": "$createdAt"
+//             }
+//         },
+//         "count": {
+//             "$sum": 1
+//         }
+//     }
+// }
+// ])
--- routes/covid19API.js
+++ routes/covid19API.js
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.post('/Cases/renderCases/caseResults', renderCasesAPIController.renderAllCases);
 router.post('/Cases/renderCases/caseResults/searchQuery', renderCasesAPIController.renderSearchCases);
+router.post('/Cases/renderCases/caseResults/statsQuery', renderCasesAPIController.statsQueryController);
 
 module.exports = router;
