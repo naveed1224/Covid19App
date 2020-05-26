eb71143324a7de1b358e3f861bb50159cd374a47
Naveed Sultan <32653946+naveed1224@users.noreply.github.com>
2020-05-25 23:54:41 -0400

    Deployment Test

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.use('/notifications', signRoutes);
 app.use('/API', covid19APIRoutes);
 
 //cron backend job
-cron.schedule("50 23 * * *", function () {
+cron.schedule("* * * * *", function () {
     let casesStats = {};
     let totalCityStat = {};
     const hostname = (req, res, next) => {
